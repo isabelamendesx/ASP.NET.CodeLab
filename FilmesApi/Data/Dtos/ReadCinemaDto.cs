6407a50a6dadfae268d40e24acb2bf718a802ca8
isabelamendesx <gomesisabela13@gmail.com>
2024-01-29 11:14:05 -0300

    (FilmesAPI): add new entity Cinema

---
+++ FilmesApi/Data/Dtos/ReadCinemaDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos
+{
+    public class ReadCinemaDto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
