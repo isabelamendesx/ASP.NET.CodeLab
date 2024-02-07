9a835efafe48ce5c5c4588c7832ad13c788a640b
Isabela Mendes <129897959+isabelamendesx@users.noreply.github.com>
2024-02-07 13:51:32 -0300

    Update Pizza.cs

--- PurplePizza-API/Models/Pizza.cs
+++ PurplePizza-API/Models/Pizza.cs
@@ -6,7 +6,6 @@ namespace PurplePizza_API.Models
     public class Pizza
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
